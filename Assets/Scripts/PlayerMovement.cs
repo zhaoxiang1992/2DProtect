e62c0853720dbeadf095efd5e001bc27451c8734
ZhaoXiang <1808724614@qq.com>
2020-10-22 23:02:24 +0800

    for prectice

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+
+    [Header("移动参数")]
+    public float speed = 8f;
+    public float crouchSpeedDivisor = 3f;
+
+    [Header("跳跃参数")]
+    public float jumpForce = 6.3f;
+    public float jumpHoldForce = 1.9f;
+    public float jumpHoldDuration = 0.1f;
+    public float crouchJumpBoost = 2.5f;
+
+    float jumpTime;
+
+    [Header("状态")]
+    public bool isCrouch;
+    public bool isOnGround;
+    public bool isJump;
+    public bool isHeadBlocked;
+
+    [Header("环境检测")]
+    public float footOffset = 0.4f;
+    public float headClearance = 0.5f;
+    public float groundDistance = 0.2f;
+    public LayerMask groundLayer;
+
+
+    float xVelocity;
+
+    //按键设置
+    bool jumpPressed;
+    bool jumpHeld;
+    bool crouchHeld;
+
+    //碰撞体尺寸
+    Vector2 colliderStandSize;
+    Vector2 colliderStandOffset;
+    Vector2 colliderCrouchSize;
+    Vector2 colliderCrouchOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+
+        colliderStandSize = coll.size;
+        colliderStandOffset = coll.offset;
+        colliderCrouchSize = new Vector2(coll.size.x, coll.size.y / 2f);
+        colliderCrouchOffset = new Vector2(coll.offset.x, coll.offset.y / 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpPressed = Input.GetButtonDown("Jump");
+        jumpHeld = Input.GetButton("Jump");
+        crouchHeld = Input.GetButton("Crouch");
+    }
+
+    private void FixedUpdate()
+    {
+        PhysicsCheck();
+        GroundMovement();
+        MidAirMovement();
+    }
+
+    void PhysicsCheck()
+    {
+        RaycastHit2D leftCheck = Raycast(new Vector2(-footOffset, 0f), Vector2.down, groundDistance, groundLayer);
+        RaycastHit2D rightCheck = Raycast(new Vector2(footOffset, 0f), Vector2.down, groundDistance, groundLayer);
+
+        if (leftCheck || rightCheck)
+            isOnGround = true;
+        else
+            isOnGround = false;
+
+        RaycastHit2D headCheck = Raycast(new Vector2(0f, coll.size.y), Vector2.up, headClearance, groundLayer);
+        if (headCheck)
+            isHeadBlocked = true;
+        else
+            isHeadBlocked = false;
+    }
+
+    void GroundMovement()
+    {
+        if (crouchHeld && !isCrouch && isOnGround)
+            Crouch();
+        else if (!crouchHeld && isCrouch && !isHeadBlocked)
+            StandUp();
+        else if (!isOnGround && isCrouch)
+            StandUp();
+
+        xVelocity = Input.GetAxis("Horizontal");//-1 ~1 之间浮点值 不按键盘时会归零
+
+        if (isCrouch)
+            xVelocity /= crouchSpeedDivisor;
+
+        rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
+
+        FilpDirection();
+    }
+
+    void MidAirMovement() {
+        if (jumpPressed && isOnGround && !isJump)
+        {
+            if (isCrouch)
+            {
+                StandUp();
+                rb.AddForce(new Vector2(0f, crouchJumpBoost), ForceMode2D.Impulse);
+            }
+
+            isOnGround = false;
+            isJump = true;
+
+            jumpTime = Time.time + jumpHoldDuration;
+
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+        else if (isJump)
+        {
+            if (jumpHeld)
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            if (jumpTime < Time.time)
+                isJump = false;
+        }
+    }
+    
+    /*设置翻转*/
+    void FilpDirection()
+    {
+        if (xVelocity < 0)
+            transform.localScale = new Vector2(-1, 1);
+
+        if (xVelocity > 0)
+            transform.localScale = new Vector2(1, 1);
+    }
+
+    void Crouch()
+    {
+        isCrouch = true;
+        coll.size = colliderCrouchSize;
+        coll.offset = colliderCrouchOffset;
+
+    }
+
+    void StandUp()
+    {
+        isCrouch = false;
+        coll.size = colliderStandSize;
+        coll.offset = colliderStandOffset;
+    }
+
+    RaycastHit2D Raycast(Vector2 offset,Vector2 rayDiraction,float length,LayerMask layer) 
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, rayDiraction, length, layer);
+        Color color = hit ? Color.red : Color.green;
+        
+        Debug.DrawRay(pos + offset, rayDiraction * length, color);
+        return hit;
+
+    }
+}
